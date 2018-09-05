ec8ccaeb94b932de7225a9a2cbdfa137a39660e4
Indu Katta <vindukatta@gmail.com>
2018-09-05 20:47:23 +0100

    updated

--- src/test/java/academy/dd/fibonacci/FibonacciTest.java
+++ src/test/java/academy/dd/fibonacci/FibonacciTest.java
@@ -6,61 +6,81 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import java.util.List;
+import java.util.Arrays;
 
-/** Created by Indu Katta 04/09/2018**/
+/** Created by Indu Katta 05/09/2018**/
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FibonacciTest {
 
     private static String testURI = "http://localhost:7003/fib/";
 
+    /*
+     * Test to check the response code for get request
+     */
     @Test
-    public void FibonacciTest(){
-
-        System.out.println("Test case to automate get request: /fib");
+    public void getfibonacciDefaultSeries(){
+        System.out.println("Test 1 : getfibonacciDefaultSeries");
         RestAssured.baseURI = testURI;
-        System.out.println("RestAssured BaseURI amended to : ["+testURI+"]");
         Response apiResponse = given().contentType("application/json").when().get();
-        int responseStatus = (int) apiResponse.statusCode();
-        System.out.println("Response status : " + responseStatus);
+        int responseStatus = apiResponse.statusCode();
         Assert.assertEquals(200,responseStatus);
-        String fibonacciList = apiResponse.asString();
-        System.out.println("Fibonacci default series : " + fibonacciList);
+        System.out.println("Fibonacci default series : " + apiResponse.asString());
+        System.out.println("Test Completed");
     }
 
+    /*
+     * Test to check the value for a given index
+     */
     @Test
-    public void FibonacciTestIndex(){
-
-        System.out.println("Test case to automate get request: /fib/range");
-        int startIndex = -10;
-        int finishIndex = 10;
+    public void testFibonacciNumberByIndex(){
+        System.out.println("Test 2 : testFibonacciNumberByIndex");
         RestAssured.baseURI = testURI;
-        System.out.println("RestAssured BaseURI amended to : ["+testURI+"]");
+        Response apiResponse = given().contentType("application/json").when()
+                .get("/29").then()
+                .statusCode(200).contentType(ContentType.JSON).
+                        extract().response();
+
+        int value = Integer.valueOf(apiResponse.asString());;
+        Assert.assertNotEquals("514229", value);
+        System.out.println("Index: 29 , value : " + value);
+        System.out.println("Test Completed");
+    }
+
+    /*
+     * Test to check the max index value
+     */
+    @Test
+    public void testFibonacciNumberOutOfRangeIndex(){
+        System.out.println("Test 3 : testFibonacciNumberOutOfRangeIndex");
+        RestAssured.baseURI = testURI;
+        Response apiResponse = given().contentType("application/json").when()
+                .get("/2147483648");
+        int responseStatus = apiResponse.statusCode();
+        Assert.assertEquals("Expected response status : 404 , Actual :"+responseStatus,404,responseStatus);
+        System.out.println("Index: 2147483648 , response code : " + responseStatus);
+        System.out.println("Test Completed");
+    }
+
+    /*
+     * Test to check the actual data returned by service
+     */
+    @Test
+    public void testFibonacciSeriesByRange(){
+        System.out.println("Test 4 : testFibonacciSeriesByRange");
+        int startIndex = 1;
+        int finishIndex = 5;
+        List<String> expectedSeries =  Arrays.asList("1","1","2","3");
+        RestAssured.baseURI = testURI;
 
         Response apiResponse = given().contentType("application/json").when()
                 .get("/range?startIndex="+startIndex+"&finishIndex="+finishIndex);
-
-        int responseStatus = (int) apiResponse.statusCode();
-        System.out.println("Response status : " + responseStatus);
-        Assert.assertEquals(200,responseStatus);
-        String fibonacciList = apiResponse.asString();
-        System.out.println("Fibonacci series within range["+startIndex+"-"+finishIndex+"] : " + fibonacciList);
-    }
-
-    @Test
-    public void FibonacciTestRange(){
-
-        System.out.println("Test case to automate get request: /fib/{index} & verifies the application failure " +
-                "with inavalid boundary value");
-
-        RestAssured.baseURI = testURI;
-        System.out.println("RestAssured BaseURI amended to : ["+testURI+"]");
-
-        Response apiResponse = given().contentType("application/json").when()
-                .get("/2147483647");
-        int responseStatus = (int) apiResponse.statusCode();
-        System.out.println("Response status : " + responseStatus);
-        Assert.assertEquals("Response status check. Expected[200], Actual["+responseStatus+"]",200,responseStatus);
+        List<String> actualSeries = apiResponse.jsonPath().getList("");
+        Assert.assertEquals(expectedSeries,actualSeries);
+        System.out.println("Fibonacci series within range["+startIndex+"-"+finishIndex+"] : " + apiResponse.asString());
+        System.out.println("Test Completed");
     }
 }
